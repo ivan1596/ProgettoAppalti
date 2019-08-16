42a54300daf3a37ebd04775f948c8cea77941ab2
ivan1596 <bollicivan@gmail.com>
2019-08-16 20:14:30 +0200

    aggiunta nuovo login funzionante

--- libretto/libretto.js
+++ libretto/libretto.js
@@ -3,3 +3,26 @@ if(utente == null){
     alert("Effettua LogIn");
     window.location.href = '../login/login.html';
 }
+
+//Procedura per connettersi alla blockchain e chiamare i metodi del contratto
+//var Web3 = require('web3');
+
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  // set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+const contract = web3.eth.contract(contractAbi);
+const contractInstance = contract.at(contractAddress);
+
+/* const transactionObject = {
+  from: fromAccount,
+  gas: gasLimit
+  gasPrice: gasPriceInWei
+}; */
+
+contractInstance.nomeMetodo.sendTransaction( transactionObject, (error, result) => { 
+    // effettuare le operazioni per gestire gli errori e i risultati 
+});
--- login/login.js
+++ login/login.js
@@ -2,18 +2,28 @@
  * Questa funzione utilizza il metodo della libreria firebase
  * per effettuare il login
  */
+
 function login(){
+    //alert("sono nel login");
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-      firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
-        sessionStorage.setItem("utente",email);
-        window.location.href = '../index/index.html';
-      }).catch(function(error) {
-        
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        window.alert(errorMessage);
-      });
+    alert(email + password);
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      sessionStorage.setItem("utente",email);
+      window.location.href = '../index/index.html';
+      
+    }).catch(function(error) {
+      
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert(errorMessage);
+    });
     
 
+}
+
+function login2(){
+  var email = "prova@prova.it";
+  var password = "provaprova";
+  
 }
