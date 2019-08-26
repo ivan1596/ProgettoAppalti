ad49618f107b63ed4b93a22b12dcca7b3aae8325
ivan1596 <bollicivan@gmail.com>
2019-08-26 16:46:55 +0200

    prove web3

--- libretto/libretto.js
+++ libretto/libretto.js
@@ -1,21 +1,22 @@
-var utente = sessionStorage.getItem("utente");
+/* var utente = sessionStorage.getItem("utente");
 if(utente == null){
     alert("Effettua LogIn");
     window.location.href = '../login/login.html';
-}
+} */
 
 //Procedura per connettersi alla blockchain e chiamare i metodi del contratto
 //var Web3 = require('web3');
 
-if (typeof web3 !== 'undefined') {
+/* if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
   // settare il provider per Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-} 
-
-const contract = web3.eth.contract(contractAbi);
-const contractInstance = contract.at(contractAddress);
+}  */
+console.log("Ciao");
+console.log(web3.isConnected());
+//const contract = web3.eth.contract(contractAbi);
+//const contractInstance = contract.at(contractAddress);
 
 /* const transactionObject = {
   from: fromAccount,
@@ -23,6 +24,6 @@ const contractInstance = contract.at(contractAddress);
   gasPrice: gasPriceInWei
 }; */
 
-contractInstance.nomeMetodo.sendTransaction( transactionObject, (error, result) => { 
+//contractInstance.nomeMetodo.sendTransaction( transactionObject, (error, result) => { 
     // effettuare le operazioni per gestire gli errori e i risultati 
-}); 
+//}); 
